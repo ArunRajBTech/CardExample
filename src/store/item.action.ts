5be438e98e1b12bd21955efdd165350820323637
arunkumar-rajendren <102282233+arunkumar-rajendren@users.noreply.github.com>
2022-10-04 13:40:28 +0530

    Resolved comments

--- src/store/item.action.ts
+++ src/store/item.action.ts
@@ -1,11 +1,16 @@
 import {createAction, props} from'@ngrx/store';
 import {Item} from '../models/item.model'
 
+export enum ActionTypes {
+    ApiSuccess = '[Item API] API success',
+    InvokeApi = '[Item API] Invoke API'
+}
+
 export const retrieveItem=createAction(
-    '[Item API] API success',
+    ActionTypes.ApiSuccess,
     props<{allItem:Item[]}>()
 );
 
 export const invokeItemAPI=createAction(
-    '[Item API] Invoke API'
+    ActionTypes.InvokeApi
 );
